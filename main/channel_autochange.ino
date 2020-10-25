784ca60ca017159a2c9d31461381946fa237c10b
fiszuuu <72563229+fiszuuu@users.noreply.github.com>
2020-10-25 18:30:20 +0100

    changing monitored channel automaticly

---
+++ main/channel_autochange.ino
@@ -0,0 +1,17 @@
+#include <user_modules.h>
+int i=1;
+#define czas 140;
+#define maxchannel 13;
+void setup() {
+  // put your setup code here, to run once:
+  wifi.monitor.start ()
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  wifi.monitor.channel(i);
+  i++;  //zmienianie kanałów wifi
+  if (i>maxchannel) //kanał o najwyższej wartości, który jest używane w Polsce
+    i=1; //nie chcemy przekroczyć tej wartości, więc wracamy spowrotem na kanał pierwszy
+  delay (czas); //sygnał zazwyczaj wysyłany jest zazwyczaj co 100 milisekund z lekkim marginesem ustawiam 140
+}
